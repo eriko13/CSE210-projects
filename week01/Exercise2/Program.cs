0f89fdf1fe6aa7110bb32b2c414755a648e4df63
eriko13 <erikgerman917@gmail.com>
2025-01-07 19:35:46 -0700

    Implement grade calculation and feedback in Program.cs

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentageStr = Console.ReadLine();
+        int gradePercentage = int.Parse(gradePercentageStr);
+
+        char letterGrade = 'F';
+        char sign = ' ';
+
+        if (gradePercentage >= 90)
+        {
+            letterGrade = 'A';
+        }
+        else if (gradePercentage >= 80)
+        {
+            letterGrade = 'B';
+        }
+        else if (gradePercentage >= 70)
+        {
+            letterGrade = 'C';
+        }
+        else if (gradePercentage >= 60)
+        {
+            letterGrade = 'D';
+        }
+
+        if (gradePercentage % 10 >= 7)
+        {
+            sign = '+';
+        }
+        else if (gradePercentage % 10 <= 3)
+        {
+            sign = '-';
+        }
+
+        if (letterGrade == 'F' || gradePercentage >= 97)
+        {
+            sign = ' ';
+        }
+
+
+
+        Console.WriteLine($"Your grade is {letterGrade} {sign}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you failed. Better luck next time!");
+        }
+
     }
 }
