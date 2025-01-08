8e8ae1689cd61757a5d9378cb255e402a4f9efd6
eriko13 <erikgerman917@gmail.com>
2025-01-07 19:19:46 -0700

    added an extra white line to output

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -9,6 +9,6 @@ class Program
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+        Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}.");
     }
 }
