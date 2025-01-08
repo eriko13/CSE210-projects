aafc3460beb36dce4f1b96a309ad96acce57916c
eriko13 <erikgerman917@gmail.com>
2025-01-07 19:52:56 -0700

    Add Number Guessing Game functionality to Program.cs

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Welcome to the Number Guessing Game!");
+        PlayGame();
+    }
+
+    static void PlayGame()
+    {
+      Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int? guess = null;
+
+        int attempts = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            attempts++;
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+        }
+
+        Console.WriteLine("\nYou guessed it!");
+        Console.WriteLine($"It took you {attempts} attempts.");
+        Console.WriteLine("\nDo you want to play again? (y/n)");
+        string playAgain = Console.ReadLine();
+
+        if (playAgain == "y")
+        {
+            PlayGame();
+        }
     }
 }
