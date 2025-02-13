3b2864bced0f964c712061ff561f9a63cf5c722c
eriko13 <erikgerman917@gmail.com>
2025-02-12 22:55:15 -0700

    Implement main menu for Mindfulness activity program

---
+++ week05/Mindfulness/ListingActivity.cs
@@ -0,0 +1,53 @@
+using System;
+
+public class ListingActivity : Activity
+{
+    private List<string> _prompts = new List<string>
+    {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    };
+
+    public ListingActivity()
+        : base("Listing Activity",
+            "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.")
+    {
+    }
+
+    public void Run()
+    {
+        DisplayStartingMessage();
+
+        Console.WriteLine();
+        string prompt = GetRandomPrompt();
+        Console.WriteLine("List as many responses as you can to the following prompt:");
+        Console.WriteLine($"--- {prompt} ---");
+        Console.Write("You may begin in: ");
+        ShowCountDown(5);
+        Console.WriteLine();
+
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(_duration);
+        int count = 0;
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("> ");
+            Console.ReadLine();
+            count++;
+        }
+
+        Console.WriteLine($"\nYou listed {count} items!");
+
+        DisplayEndingMessage();
+    }
+
+    private string GetRandomPrompt()
+    {
+        Random random = new Random();
+        return _prompts[random.Next(_prompts.Count)];
+    }
+} 
